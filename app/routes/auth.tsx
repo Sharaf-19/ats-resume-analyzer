f6572241870db1bdd6d956ed6fa6f3dcd2578c95
Sharaf <sherefbarcelona@gmail.com>
2025-11-23 19:48:45 +0300

    minor fixes

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -3,7 +3,7 @@ import { useLocation, useNavigate } from 'react-router';
 import { usePuterStore } from '~/lib/puter';
 
 export const meta = () => [
-  { title: 'Resumind | Auth' },
+  { title: 'Resumyzer | Auth' },
   { name: 'description', content: 'Login to your account' },
 ];
 
